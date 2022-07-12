bc928e6b9869a772ff44ebb15f39c5a0caa2cdac
boldyrevda <22929109+BoldyrevDA@users.noreply.github.com>
2022-07-12 23:05:08 +0400

    feat: add types

--- product-service/src/handlers/getProductsById.ts
+++ product-service/src/handlers/getProductsById.ts
@@ -1,6 +1,8 @@
+import { Context, APIGatewayEvent } from 'aws-lambda';
+import { Product } from "types/product";
 import products from 'mocks/products.json';
 
-export async function getProductById(event) {
+export async function getProductById(event: APIGatewayEvent, context: Context) {
     const { productId } = event.pathParameters;
     const product = products.find((product) => product.id === productId)
     const response = product === undefined ? null : product;
