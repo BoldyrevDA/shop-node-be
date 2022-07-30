f1b1268356202195c248aa08a38b7994507ad3c0
boldyrevda <22929109+BoldyrevDA@users.noreply.github.com>
2022-07-30 23:47:20 +0400

    feat: implement importFileParser handler

--- import-service/src/config.ts
+++ import-service/src/config.ts
@@ -1,3 +1,7 @@
 export default {
-    JEST: process.env.JEST === 'true'
+    JEST: process.env.JEST === 'true',
+    BUCKET: process.env.BUCKET,
+    REGION: process.env.REGION,
+    UPLOADED_CATALOG: process.env.UPLOADED_CATALOG,
+    PARSED_CATALOG: process.env.PARSED_CATALOG,
 }
--- import-service/src/handlers/importProductsFile.ts
+++ import-service/src/handlers/importProductsFile.ts
@@ -3,13 +3,10 @@ import { APIGatewayEvent } from "aws-lambda";
 import {lambdaHandler} from "utils/lamdaHandler";
 import {HttpCode, HttpError} from "product-service/src/utils/http";
 import {validateFileName} from "utils/validators/upload-validators";
-
-const region = 'eu-west-1';
-const BUCKET = 'shop-files-bucket';
-const catalog = 'uploaded'
+import config from "config";
 
 export const importProductsFile = lambdaHandler(async (event: APIGatewayEvent) => {
-    const s3 = new AWS.S3({ region })
+    const s3 = new AWS.S3({ region: config.REGION })
     const fileName = event.queryStringParameters?.name;
 
     const validateError = validateFileName(fileName);
@@ -21,8 +18,8 @@ export const importProductsFile = lambdaHandler(async (event: APIGatewayEvent) =
     }
 
     const params = {
-        Bucket: BUCKET,
-        Key: `${catalog}/${fileName}`,
+        Bucket: config.BUCKET,
+        Key: `${config.UPLOADED_CATALOG}/${fileName}`,
         Expires: 60,
         ContentType: 'text/csv',
     }
